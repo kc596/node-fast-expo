3af6ac194e0865cd916f2a54d10c22c47e7fc0b4
kunal.chau <kunal.chau@media.net>
2020-09-11 17:12:03 +0530

    Adds something useless for polyfill check in older browsers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-export function expo(a: number, b: number) : number {
+export function expo(a: number, b: number): number {
     if (!isFinite(a) || !isFinite(b)) { return NaN; }
-    let ans : number = 1;
+    let ans: number = 1;
     while (b > 0) {
-        if (b%2 === 1) {
+        if (b % 2 === 1) {
             ans *= a;
         }
         a *= a;
-        b = Math.floor(b/2);
+        b = Math.floor(b / 2);
     }
+    Promise.resolve().finally();
     return ans;
+}
+
+export function cont(a: Array<string>, b: string): boolean {
+    return a.includes(b);
 }
